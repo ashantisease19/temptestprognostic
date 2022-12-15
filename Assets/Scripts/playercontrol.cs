a6aefc7619dc7e6e1177ade523e3191d11e0a575
ashantisease19 <77317067+ashantisease19@users.noreply.github.com>
2022-12-15 04:37:48 -0500

    iced coffee break

--- Assets/Scripts/playercontrol.cs
+++ Assets/Scripts/playercontrol.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class playercontrol : MonoBehaviour
 {
+    public float speed;
+    public float jumpSpeed = 5;
+    public GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(transform.position.x);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        float xMove = Input.GetAxis("Horizontal");
         
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(0, jumpSpeed, 0);
+        }
     }
 }
+    
